cb5921d6909b438a7902ae562c08a4f14074bfa9
Shivam Kumar <shivam34.kumar@ril.com>
2024-08-20 17:52:01 +0530

    added changes for queryTemporaryPosts

--- src/controllers/temporarypost.controller.js
+++ src/controllers/temporarypost.controller.js
@@ -20,7 +20,7 @@ const createBulkTemporaryPost = catchAsync(async (req, res) => {
 const getTemporaryPost = catchAsync(async (req, res) => {
     const filter = pick(req.query, ['category_id']);
     const options = pick(req.query, ['sortBy', 'limit', 'page']);
-    const result = await temporaryPostService.queryTemporaryPost(filter, options);
+    const result = await temporaryPostService.queryTemporaryPosts(filter, options);
     res.send(result);
   });
   
